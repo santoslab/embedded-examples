bf4b5fc4f243b26b772b96a956a62ac1f5fc7e15
John Hatcliff <hatcliff@ksu.edu>
2018-12-19 14:44:24 -0600

    Update SST examples

--- playground/jvm/src/test/scala/org/sireum/playground/StringTemplateTests.scala
+++ playground/jvm/src/test/scala/org/sireum/playground/StringTemplateTests.scala
@@ -6,10 +6,51 @@ import org.sireum.test._
 class StringTemplateTests extends TestSuite {
 
   val tests = Tests {
+    // * adds position info
+    // - registers test as JUnit tests
     * - {
       val str = "Hello world"
       println(str)
     }
+
+    // Parr example
+    //   import org.stringtemplate.v4.*;
+    //   ...
+    //   ST hello = new ST("Hello, <name>");
+    //   hello.add("name", "World");
+    //   System.out.println(hello.render());
+
+    * - {
+      val name: String = "World"
+      val hello: ST = st"Hello, ${name}!"
+      println(hello.render)
+    }
+
+
+    * - {
+      def hello(name:String): ST = st"Hello, ${name}!"
+      println(hello("World").render)
+    }
+
+    * - {
+      def sideByside(text:String): ST = st"${text}::${text}"
+      def hello(name:String): ST = st"Hello, ${sideByside(name).render}!"
+      println(hello("World").render)
+    }
+
+    * - {
+      def sideByside(text:String): ST = st"${text}::${text}"
+      def hello(name:String): ST = st"Hello, ${sideByside(name)}!"
+      println(hello("World").render)
+    }
+
+    //    * - {
+//      def init(v:String) = if OT NULLXXX st"= ${v}" else st""
+//      def decl(typ: String, name: String, value: String)
+//             = st"${typ} ${name} ${init(value)}"
+//      // file /tmp/init.st
+//
+//    }
 
     * - {
       val str = "Hello world - single line string template"
@@ -49,11 +90,34 @@ class StringTemplateTests extends TestSuite {
       _st.render.native == expected
     })
 
-    *({
-      val expected = "1,2,3"
-      val _st = st"""${(ISZ('a', 'b', 'c'), ",")}"""
-      println(s"Does '${_st.render}' == '${expected}'?")
-      _st.render.native == expected
-    }, "Example error message")
-  }
+//    *({
+//      val expected = "1,2,3"
+//      val _st = st"""${(ISZ('a', 'b', 'c'), ",")}"""
+//      println(s"Does '${_st.render}' == '${expected}'?")
+//      _st.render.native == expected
+//    }, "Example error message")
+
+def xTaskCreateST(taskFunctionName:String, taskName:String,
+                  taskStackSize:String, taskPV:String,
+                  taskPriority:String, taskHandle: String) =
+  st""" // see https://docs.aws.amazon.com/freertos-kernel/latest/ref/reference5.html
+xTaskCreate(
+    ${taskFunctionName}, // name of task function
+    (const char* const) ${taskName}, // task name
+    ${taskStackSize}, // Stack size
+    ${taskPV}, // task parameters values
+    ${taskPriority}, // priority
+    ${taskHandle}); // task handle"""
+
+    * - { val taskFunctionName = "MyTask"
+          val taskID = "MyTaskID"
+          val stackSize = "3"
+          val taskPV = "NULL"
+          val priority = "2"
+          val taskHandle = "NULL"
+          val _st = xTaskCreateST(taskFunctionName, taskID,
+                                  stackSize, taskPV, priority, taskHandle)
+            println(_st.render)
+              }
+    }
 }
