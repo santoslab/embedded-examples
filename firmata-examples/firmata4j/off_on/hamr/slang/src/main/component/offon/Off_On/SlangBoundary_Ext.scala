7b382a6a19d6c3fc23dc908c81167fc0dffcfa6a
Jason Belt <belt@ksu.edu>
2023-10-11 17:16:51 -0500

    tweak

--- firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/SlangBoundary_Ext.scala
+++ firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/SlangBoundary_Ext.scala
@@ -11,17 +11,18 @@ object SlangBoundary_Ext {
 
     if (!DeviceBridge.ready) {
       val deviceBridge = Os.path(implicitly[sourcecode.File].value).up / "device" / "DeviceBridge.scala"
-      println(s"Device not found on ${DeviceBridge.port} as specified in ${deviceBridge.toUri}")
+      println(s"\nDevice not found on ${DeviceBridge.port} as specified in ${deviceBridge.toUri}")
       println("\nHere are the connected serial devices:")
       for(p <- SerialPort.getCommPorts.toList) {
         println(s"  ${p.getSystemPortPath}${if (p.getVendorID == 0x2341) " by Arduino LLC" else ""}")
       }
-      print("\nWill simulate devices instead starting in 5 seconds")
+      println("\nNote: You may need to give rwx access to the device (e.g. sudo chmod 777 /dev/ttyACM0)")
+      print(s"\nWill simulate devices instead starting in 5 seconds  ")
       for (i <- 4 to 0 by -1) {
         Thread.sleep(1000)
-        print(s"\b\b\b\b\b\b\b\b\b$i seconds")
+        print(s"\b\b\b\b\b\b\b\b\b\b\b$i seconds ${if (i % 2 == 0) "*" else " "}")
       }
-      println()
+      println("\b ")
     }
   }
 
