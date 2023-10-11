0a258728ab8a511aa44405f0a0ac2e24dd942b94
Jason Belt <belt@ksu.edu>
2023-10-11 16:18:16 -0500

    add countdown to simulation

--- firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/SlangBoundary_Ext.scala
+++ firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/SlangBoundary_Ext.scala
@@ -10,12 +10,18 @@ object SlangBoundary_Ext {
     DeviceBridge.init()
 
     if (!DeviceBridge.ready) {
-      println(s"Device not found on ${DeviceBridge.port}")
+      val deviceBridge = Os.path(implicitly[sourcecode.File].value).up / "device" / "DeviceBridge.scala"
+      println(s"Device not found on ${DeviceBridge.port} as specified in ${deviceBridge.toUri}")
       println("\nHere are the connected serial devices:")
       for(p <- SerialPort.getCommPorts.toList) {
         println(s"  ${p.getSystemPortPath}${if (p.getVendorID == 0x2341) " by Arduino LLC" else ""}")
       }
-      println("\nWill simulate device instead")
+      print("\nWill simulate devices instead starting in 5 seconds")
+      for (i <- 4 to 0 by -1) {
+        Thread.sleep(1000)
+        print(s"\b\b\b\b\b\b\b\b\b$i seconds")
+      }
+      println()
     }
   }
 
