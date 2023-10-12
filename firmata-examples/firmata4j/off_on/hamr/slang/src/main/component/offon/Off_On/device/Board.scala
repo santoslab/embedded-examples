4e062715aa4e63f5da88d900d629fc775e143a8a
Jason Belt <belt@ksu.edu>
2023-10-12 11:08:50 -0500

    add back pinboard

--- firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/device/Board.scala
+++ firmata-examples/firmata4j/off_on/hamr/slang/src/main/component/offon/Off_On/device/Board.scala
@@ -4,6 +4,12 @@ import org.sireum._
 import org.firmata4j._
 import org.firmata4j.firmata._
 import FirmataUtil._
+import org.firmata4j.ui.JPinboard
+
+import java.awt.event.{WindowAdapter, WindowEvent}
+import java.awt.{GridBagConstraints, GridBagLayout}
+import java.io.IOException
+import javax.swing.{JFrame, SwingUtilities, WindowConstants}
 
 object Board {
 
@@ -12,6 +18,7 @@ object Board {
   var initRun: B = F
 
   def init(port: String): Unit = {
+    this.port = port
     this.synchronized {
       try {
         if (!initRun) {
@@ -19,6 +26,7 @@ object Board {
           device = new FirmataDevice(port.native)
           device.start()
           device.ensureInitializationIsDone()
+          initPinboard(device)
         }
       } catch {
         case e: Throwable =>
@@ -43,5 +51,36 @@ object Board {
     val p = device.getPin(pin.toInt)
     p.setMode(Pin.Mode.valueOf(mode.name.native))
     p.setValue(value.toInt)
+  }
+
+  def initPinboard(device: IODevice): Unit = {
+    SwingUtilities.invokeLater(new Runnable() {
+      override def run(): Unit = {
+        val mainFrame = new JFrame(port.native)
+        val layout = new GridBagLayout()
+        val constraints = new GridBagConstraints()
+        mainFrame.setLayout(layout)
+        constraints.gridy = 0
+        constraints.fill = GridBagConstraints.BOTH
+        constraints.weightx = 1
+        constraints.weighty = 1
+        val pinboard = new JPinboard(device)
+        layout.setConstraints(pinboard, constraints)
+        mainFrame.add(pinboard)
+        mainFrame.pack
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+        mainFrame.addWindowListener(new WindowAdapter() {
+          override def windowClosing(e: WindowEvent): Unit = {
+            try device.stop
+            catch {
+              case ex: IOException =>
+                throw new RuntimeException(ex)
+            }
+            super.windowClosing(e)
+          }
+        })
+        mainFrame.setVisible(true)
+      }
+    })
   }
 }
